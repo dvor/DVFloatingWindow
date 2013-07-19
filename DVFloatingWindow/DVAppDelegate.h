3a4735dd6a6aa07e9f369a64f7e873a2f3282ab0
Dmitry Vorobyov <dvor@dvor.me>
2013-07-19 23:38:58 +0300

    Initial commit

---
+++ DVFloatingWindow/DVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DVAppDelegate.h
+//  DVFloatingWindow
+//
+//  Created by Dmitry Vorobyov on 7/19/13.
+//  Copyright (c) 2013 Dmitry Vorobyov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DVFloatingWindow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DVFloatingWindow
+//
+//  Created by Dmitry Vorobyov on 7/19/13.
+//  Copyright (c) 2013 Dmitry Vorobyov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DVAppDelegate class]));
+    }
+}
